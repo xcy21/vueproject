840bb6c739e46ad850e4ac649116dd31f131ef11
xinciyuan <1487994375@qq.com>
2020-11-08 22:46:22 +0800

    添加login模块

--- vuetest/src/router/index.js
+++ vuetest/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+export const constantRouterMap = [
+   //配置默认的路径，默认显示登录页
+   { path: '/', component: () => import('@/views/login')},
+
+   //配置登录成功页面，使用时需要使用 path 路径来实现跳转
+   { path: '/success', component: () => import('@/views/success')},
+
+   //配置登录失败页面，使用时需要使用 path 路径来实现跳转
+   { path: '/error', component: () => import('@/views/error'), hidden: true }
+]
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap //指定路由列表
 })
---
+++ vuetest/src/views/success.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Welcome{{msg}}</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+    msg: this.$route.query.data
+    };
+  }
+}
+</script>
